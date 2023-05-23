5f35949624b9b7bad0f129b62fa7c6ece1e51326
Yushenko Dima <damios.1812@gmail.com>
2023-05-23 18:30:15 +0300

    first commit 2

--- js/timer.js
+++ js/timer.js
@@ -1,35 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // конечная дата, например 1 декабря 2021
-    const deadline = new Date(2023, 24, 05);
-    // id таймера
-    let timerId = null;
-    // склонение числительных
-    function declensionNum(num, words) {
+
+// класс для создание таймера обратного отсчета
+class CountdownTimer {
+    constructor(deadline, cbChange, cbComplete) {
+        this._deadline = deadline;
+        this._cbChange = cbChange;
+        this._cbComplete = cbComplete;
+        this._timerId = null;
+        this._out = {
+            days: '', hours: '', minutes: '', seconds: '',
+            daysTitle: '', hoursTitle: '', minutesTitle: '', secondsTitle: ''
+        };
+        this._start();
+    }
+    static declensionNum(num, words) {
         return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
     }
-    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
-    function countdownTimer() {
-        const diff = deadline - new Date();
-        if (diff <= 0) {
-            clearInterval(timerId);
-        }
-        const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+    _start() {
+        this._calc();
+        this._timerId = setInterval(this._calc.bind(this), 1000);
+    }
+    _calc() {
+        const diff = this._deadline - new Date();
         const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
         const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
         const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-        $hours.dataset.title = declensionNum(hours, ['tunti', 'tuntia', 'tunteja']);
-        $minutes.dataset.title = declensionNum(minutes, ['minuutti', 'minuuttia', 'minuutteja']);
-        $seconds.dataset.title = declensionNum(seconds, ['sekunti', 'sekuntia', 'sekunteja']);
-        $hours.textContent = hours < 10 ? '0' + hours : hours;
-        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
-        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+        this._out.hours = hours < 10 ? '0' + hours : hours;
+        this._out.minutes = minutes < 10 ? '0' + minutes : minutes;
+        this._out.seconds = seconds < 10 ? '0' + seconds : seconds;
+        this._out.hoursTitle = CountdownTimer.declensionNum(hours, ['tunti', 'tuntia', 'tunteja']);
+        this._out.minutesTitle = CountdownTimer.declensionNum(minutes, ['minuutti', 'minuuttia', 'minuutteja']);
+        this._out.secondsTitle = CountdownTimer.declensionNum(seconds, ['sekunti', 'sekuntia', 'sekunteja']);
+        this._cbChange ? this._cbChange(this._out) : null;
+        if (diff <= 0) {
+            clearInterval(this._timerId);
+            this._cbComplete ? this._cbComplete() : null;
+        }
     }
-    // получаем элементы, содержащие компоненты даты
-    const $hours = document.querySelector('.timer__hours');
-    const $minutes = document.querySelector('.timer__minutes');
-    const $seconds = document.querySelector('.timer__seconds');
-    // вызываем функцию countdownTimer
-    countdownTimer();
-    // вызываем функцию countdownTimer каждую секунду
-    timerId = setInterval(countdownTimer, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // .timer (на минуту)
+    const elHours = document.querySelector('.timer .timer__hours');
+    const elMinutes = document.querySelector('.timer .timer__minutes');
+    const elSeconds = document.querySelector('.timer .timer__seconds');
+
+    (function timer() {
+        const deadline = new Date(Date.now() + (15 * 60 * 1000 + 999));
+        new CountdownTimer(deadline, (timer) => {
+            elHours.textContent = timer.hours;
+            elMinutes.textContent = timer.minutes;
+            elSeconds.textContent = timer.seconds;
+            elHours.dataset.title = timer.hoursTitle;
+            elMinutes.dataset.title = timer.minutesTitle;
+            elSeconds.dataset.title = timer.secondsTitle;
+        }, () => {
+            timer();
+        })
+    })();
+
 });
