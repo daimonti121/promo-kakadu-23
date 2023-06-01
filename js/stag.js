e17813d80b23f9b4fa36eb9225ba6c029ebceff3
Yushenko Dima <damios.1812@gmail.com>
2023-06-01 15:48:10 +0300

    add new fil

--- js/stag.js
+++ js/stag.js
@@ -3,7 +3,7 @@ function $_GET(e,t){return!!(t=t.match(new RegExp(e+"=([^&=]+)")))&&t[1]}
     var stag = "?stag=" + getStag;
     console.log("STAG - " + stag);
 
-    $('.stag').click(function(event){
+    $('#stag').click(function(event){
         event.preventDefault();
         event.stopImmediatePropagation();
         window.location.href = 'https://casinokakadu.com/fi' + stag;
